86fa30a5ccdf3b5aa27c58b520459f1eca7bf721
Arvind Ganesh <arvind.cganesh@gmail.com>
2016-09-28 22:26:54 +0530

    Added gradients

--- MandelbrotSet.py
+++ MandelbrotSet.py
@@ -11,15 +11,16 @@ def iterate(x, y, iterationNum):
         z = z * z + coord
         if abs(z) > 2:
             return False
-    return True
+    return abs(z)
+
 
 def draw(grid):
     img = Image.new('RGB', (len(grid), len(grid)), "white")
     pixels = img.load()
     for row in xrange(len(grid)):
         for col in xrange(len(grid[row])):
-            if grid[row][col] == True:
-                pixels[row, col] = (0, 0, 255)
+            if grid[row][col] != False:
+                pixels[row, col] = grid[row][col]
     return img
 
 def mandelbrot():
@@ -31,7 +32,7 @@ def mandelbrot():
     h = TopLeftY - BottomRightY
     #You needed to make sure these are ints!
     #Can't create an array with decimal size
-    npArr = np.zeros((int(w / increment), int(h / increment)), dtype=bool)
+    npArr = np.zeros((int(w / increment), int(h / increment)), dtype=object)
     #Use the increment variable from above. It won't work with xrange because that doesn't
     #Support decimals. You probably want to use a while loop or something
     x = -2
@@ -40,23 +41,23 @@ def mandelbrot():
     while TopLeftX <= x <= BottomRightX:
         y = 2
         while TopLeftY >= y >= BottomRightY:
-            if iterate(x, y, maxIt):
+            if not not iterate(x, y, maxIt):
                 #So this is where the biggest issue is...
                 #Your x is going to be between -2 and 2 (as a decimal)
                 #But the array has values between 0 and w / increment
                 #So we need to convert back to the npArr coordinate system
-                
+
                 #This is what you had:
                 #npArr[x][y] = True
-                
+
                 #This is what it should be:
-                npArr[int((x - TopLeftX) / increment)][int((y - BottomRightY) / increment)] = True
+                npArr[int((x - TopLeftX) / increment)][int((y - BottomRightY) / increment)] = (0,int(127.5 * iterate(x, y, maxIt)), 255 - int(127.5 * iterate(x, y, maxIt)))
                 #Think of it this way... TopLeftX should be 0 in the array and BottomLeftX should be w / increment
                 #So to convert them, take x - TopLeftX and then expand it to be the size of the array (/ increment)
                 count += 1
             y -= increment
         x += increment
     return npArr
-
+#print mandelbrot().index("True")
 img = draw(mandelbrot())
 img.save("mandelbrot.png")
